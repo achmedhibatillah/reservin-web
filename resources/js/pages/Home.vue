93a868c5c079bad2e8f3ad8b5a4d78b98bddbd3a
achmedhibatillah <ach.hibatillah@gmail.com>
2025-05-06 20:32:17 +0700

    First!

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NavMain from '../components/NavMain.vue';
+import NavFooter from '../components/NavFooter.vue';
+
+const items = [
+  { title: 'Dashboard', href: '/', icon: 'HomeIcon' },
+  { title: 'Settings', href: '/settings', icon: 'SettingsIcon' }
+];
+</script>
+
+<template>
+    <NavMain :items="items" />
+    <div class="p-6">
+        <h1 class="text-2xl font-bold">Halo dari Halaman Home.vue!</h1>
+    </div>
+    <NavFooter />
+</template>
