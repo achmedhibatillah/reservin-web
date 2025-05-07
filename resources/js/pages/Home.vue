b3a23e9a5a3fdd44400534d43baafae7c79ec6ff
achmedhibatillah <ach.hibatillah@gmail.com>
2025-05-07 18:08:30 +0700

    Updated!

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -2,6 +2,8 @@
 import NavMain from '../components/NavMain.vue';
 import NavFooter from '../components/NavFooter.vue';
 
+import Button from '../components/ui/button/Button.vue'
+
 const items = [
   { title: 'Dashboard', href: '/', icon: 'HomeIcon' },
   { title: 'Settings', href: '/settings', icon: 'SettingsIcon' }
@@ -9,9 +11,10 @@ const items = [
 </script>
 
 <template>
-    <NavMain :items="items" />
+
+    <Button />
     <div class="p-6">
-        <h1 class="text-2xl font-bold">Halo dari Halaman Home.vue!</h1>
+        <h1 class="text-2xl font-bold">Halo dari Halaman Home vue!</h1>
     </div>
     <NavFooter />
 </template>
