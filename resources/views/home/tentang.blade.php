b79e579ec5b74c0be2f1d3ca6d4ec0d77dfd9edb
achmedhibatillah <ach.hibatillah@gmail.com>
2025-06-07 22:52:53 +0700

    Updated!

--- resources/views/home/tentang.blade.php
+++ resources/views/home/tentang.blade.php
@@ -1,6 +1,23 @@
-<div class="bg-clr1" style="padding: 200px 0 100px 0">
+<div class="bg-clr1" style="padding: 0 0 100px 0">
+    <div class="w-100 bg-web d-flex flex-column justify-content-center align-items-center position-relative overflow-hidden" 
+    style="height:300px; background-image:url('assets/images/static/team/bg-full.png'); background-size:cover; background-position:center;">
+        <div class="d-flex flex-column align-items-center position-relative z-1 pt-4">
+            <h1 class="bg-clr2 text-clr1 rounded-pill fsz-28 px-4 py-2 text-center fw-bold m-0 mb-2 mt-5">TENTANG KAMI</h1>
+            <p class="text-light text-center m-0 fsz-10">Profil pengembang Sistem Informasi ReservIN.</p>
+        </div>
+
+        <div style="
+            position: absolute;
+            bottom: 0;
+            width: 100%;
+            height: 100px;
+            background: linear-gradient(to bottom, rgba(29, 41, 50, 0) 0%, rgba(29, 41, 50, 0.3) 50%, rgba(29, 41, 50, 1) 100%);
+            backdrop-filter: blur(1px);
+            pointer-events: none;
+        "></div>
+    </div>
+
     <div class="container">
-        <h2 class="text-light text-center fw-bold">TENTANG KAMI</h2>
         <div class="row justify-content-center">
             <div class="col-md-6 col-lg-4 d-flex justify-content-center">
                 <div class="card position-relative rounded-l mb-3 p-3 shadow-l" style="margin-top:140px;width:320px;background:linear-gradient(to top left, #b1c7e3, #f1f1f1)">
@@ -26,13 +43,13 @@
             <div class="col-md-6 col-lg-4 d-flex justify-content-center">
                 <div class="card position-relative rounded-l mb-3 p-3 shadow-l" style="margin-top:140px;width:320px;background:linear-gradient(to top left, #b1c7e3, #f1f1f1)">
                     <div class="position-absolute translate-center d-flex justify-content-center align-items-center overflow-hidden rounded-circle rounded square shadow-l" style="border:2px solid var(--clr3);width:150px;left:50%">
-                        <img src="{{ asset('assets/images/static/team/regga.PNG') }}" class="img-cover">
+                        <img src="{{ asset('assets/images/static/team/ahmed.PNG') }}" class="img-cover">
                     </div>
                     <div class="" style="margin-top:100px">
-                        <h4 class="text-center text-clr1 fw-bold lh-1">Regga Ananda<br>Firdaus</h4>
+                        <h4 class="text-center text-clr1 fw-bold lh-1">Achmed<br>Hibatillah</h4>
                         <p class="text-secondary d-flex align-items-center justify-content-center m-0 fsz-10">S1 Teknologi Informasi</p>
                         <p class="text-center text-secondary fsz-10">2022</p>
-                        <p class="text-center text-clr2 font-latin fsz-16">UI/UX Mobile Designer</p>
+                        <p class="text-center text-clr2 font-latin fsz-16">Full-stack web developer</p>
                         <div class="row px-3" style="margin-top:30px;">
                             <div class="col-6 p-0 pe-1">
                                 <a href="https://" target="_blank" class="btn btn-clr1 text-light w-100"><i class="fab fa-github me-2"></i> Github</a>
@@ -47,13 +64,13 @@
             <div class="col-md-6 col-lg-4 d-flex justify-content-center">
                 <div class="card position-relative rounded-l mb-3 p-3 shadow-l" style="margin-top:140px;width:320px;background:linear-gradient(to top left, #b1c7e3, #f1f1f1)">
                     <div class="position-absolute translate-center d-flex justify-content-center align-items-center overflow-hidden rounded-circle rounded square shadow-l" style="border:2px solid var(--clr3);width:150px;left:50%">
-                        <img src="{{ asset('assets/images/static/team/ahmed.PNG') }}" class="img-cover">
+                        <img src="{{ asset('assets/images/static/team/regga.PNG') }}" class="img-cover">
                     </div>
                     <div class="" style="margin-top:100px">
-                        <h4 class="text-center text-clr1 fw-bold lh-1">Achmed<br>Hibatillah</h4>
+                        <h4 class="text-center text-clr1 fw-bold lh-1">Regga Ananda<br>Firdaus</h4>
                         <p class="text-secondary d-flex align-items-center justify-content-center m-0 fsz-10">S1 Teknologi Informasi</p>
                         <p class="text-center text-secondary fsz-10">2022</p>
-                        <p class="text-center text-clr2 font-latin fsz-16">Full-stack web developer</p>
+                        <p class="text-center text-clr2 font-latin fsz-16">UI/UX Mobile Designer</p>
                         <div class="row px-3" style="margin-top:30px;">
                             <div class="col-6 p-0 pe-1">
                                 <a href="https://" target="_blank" class="btn btn-clr1 text-light w-100"><i class="fab fa-github me-2"></i> Github</a>
